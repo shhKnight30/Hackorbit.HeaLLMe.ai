01caf089f71a4628f1090fbeab1acc15fd04365b
shhKnight30 <aishwarybatt@gmail.com>
2025-07-08 13:09:02 +0530

    mongodb connection exstablished

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+import connectDB from "./config/db/index.js";
+import {app} from './app.js'
+
+dotenv.config({
+    path: '../.env'
+})
+
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {    
+        console.log(`Server is running on port ${process.env.PORT}`);
+    }
+    );
+}).catch((error) => {
+    console.error("Error starting the server:", error);
+    process.exit(1); 
+}
+);
